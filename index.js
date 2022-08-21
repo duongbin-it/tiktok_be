65077ae6c4a34ade487a1ddd711d8d01a0e77656
Nguyễn Bình Dương <98762397+duongbin-it@users.noreply.github.com>
2022-08-21 22:34:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ mongo.connect((err, db) => {
               name_music: req.body.name_music,
               heart_check: req.body.heart_check,
             })
+          res.json('post success!');
         }
       })
   })
@@ -93,6 +94,7 @@ mongo.connect((err, db) => {
         following: req.body.following,
         avatar: req.body.avatar
       })
+     res.json('post success!');
   })
 
   app.post("/api/users", (req, res) => {
